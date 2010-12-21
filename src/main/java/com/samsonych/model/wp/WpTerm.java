cea9880d26b19d17a7949c24a86fb855687c9bc4
samsonych <samsserg@gmail.com>
2010-12-21 18:23:43 +0000

    git-svn-id: svn://samsonov.isida.by/trunk/com.samsonych.wpgads@32 cbd229a6-c238-436f-88bf-a8155ce9037e

---
+++ src/main/java/com/samsonych/model/wp/WpTerm.java
@@ -0,0 +1,73 @@
+package com.samsonych.model.wp;
+
+// Generated 21.12.2010 7:24:07 by Hibernate Tools 3.4.0.Beta1
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+/**
+ * The categories for both posts and links and the tags for posts.
+ */
+@Entity
+@Table(name = "wp_terms", uniqueConstraints = @UniqueConstraint(columnNames = "slug"))
+public class WpTerm implements java.io.Serializable {
+
+	private static final long serialVersionUID = 4860621151071891850L;
+	private Long termId;
+	private String name;
+	private String slug;
+	private long termGroup;
+
+	public WpTerm() {
+	}
+
+	public WpTerm(String name, String slug, long termGroup) {
+		this.name = name;
+		this.slug = slug;
+		this.termGroup = termGroup;
+	}
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "term_id", unique = true, nullable = false)
+	public Long getTermId() {
+		return this.termId;
+	}
+
+	public void setTermId(Long termId) {
+		this.termId = termId;
+	}
+
+	@Column(name = "name", nullable = false, length = 200)
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "slug", unique = true, nullable = false, length = 200)
+	public String getSlug() {
+		return this.slug;
+	}
+
+	public void setSlug(String slug) {
+		this.slug = slug;
+	}
+
+	@Column(name = "term_group", nullable = false)
+	public long getTermGroup() {
+		return this.termGroup;
+	}
+
+	public void setTermGroup(long termGroup) {
+		this.termGroup = termGroup;
+	}
+
+}
---
+++ src/main/java/com/samsonych/model/wp/WpTermRelationship.java
@@ -0,0 +1,53 @@
+package com.samsonych.model.wp;
+
+// Generated 21.12.2010 7:24:07 by Hibernate Tools 3.4.0.Beta1
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Posts are associated with categories and tags from the wp_terms table and
+ * this association.
+ */
+@Entity
+@Table(name = "wp_term_relationships")
+public class WpTermRelationship implements java.io.Serializable {
+
+	private static final long serialVersionUID = 9159480990706478868L;
+	private WpTermRelationshipId id;
+	private int termOrder;
+
+	public WpTermRelationship() {
+	}
+
+	public WpTermRelationship(WpTermRelationshipId id, int termOrder) {
+		this.id = id;
+		this.termOrder = termOrder;
+	}
+
+	@EmbeddedId
+	@AttributeOverrides({
+			@AttributeOverride(name = "objectId", column = @Column(name = "object_id", nullable = false)),
+			@AttributeOverride(name = "termTaxonomyId", column = @Column(name = "term_taxonomy_id", nullable = false)) })
+	public WpTermRelationshipId getId() {
+		return this.id;
+	}
+
+	public void setId(WpTermRelationshipId id) {
+		this.id = id;
+	}
+
+	@Column(name = "term_order", nullable = false)
+	public int getTermOrder() {
+		return this.termOrder;
+	}
+
+	public void setTermOrder(int termOrder) {
+		this.termOrder = termOrder;
+	}
+
+}
